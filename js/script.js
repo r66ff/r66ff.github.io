3e19945f0b193752e14fafcaa4754c3a6eceaeef
Antonina Serdyukova <r66ff@hotmail.com>
2017-10-04 23:04:03 -0400

    started styling

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
         // Only prevent default if animation is actually gonna happen
         event.preventDefault();
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 70
         }, 1000, function() {
           // Callback after animation
           // Must change focus!
